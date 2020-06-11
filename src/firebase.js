b76dcf86ae947b3e97ff81b151b15f15a518de82
JuanDiego6998 <juandiegogarzon98@hotmail.com>
2020-06-10 19:32:07 -0500

    firebase
    firebase implementado en main.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBiu_OJ9_nTVITZ9fYSeg7627rM_c2-Hbg",
+    authDomain: "regeneracionoceanos.firebaseapp.com",
+    databaseURL: "https://regeneracionoceanos.firebaseio.com",
+    projectId: "regeneracionoceanos",
+    storageBucket: "regeneracionoceanos.appspot.com",
+    messagingSenderId: "1058872195185",
+    appId: "1:1058872195185:web:6a32955dd5bc57ea3ec3d0",
+    measurementId: "G-79D0P1B8J7"
+  };
+
+firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import 'bootstrap';
-import jQuery from'jquery';
+import jQuery from 'jquery';
 
-window.$=window.jQuery=jQuery;
+window.$ = window.jQuery = jQuery;
 
 import 'popper.js';
-import './assets/app.scss';
+import './assets/css/app.scss';
 
-Vue.component('Sidebar',require('./components/sidebar.vue').default);  
+Vue.component('Sidebar', require('./components/sidebar.vue').default);
 
 
 Vue.config.productionTip = false;
