4b67329fdb75339e61b4a952faf6606ab92f0c41
Ignacio <ignacio.vergara282@gmail.com>
2020-06-20 09:53:59 -0500

    cambio de color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,49 @@
 <template>
-    <b-row id="logos" class="justify-content-md-right" align-v="end" align-h="center">
+  <section id="footer">
+    <div class="container">
+      <b-row>
+        <b-row id="logos" class="justify-content-md-right" align-v="end" align-h="center">
+        <b-col cols="12" md="4" class="pb-4">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+        </b-col>
+        <b-col id="logos" cols="12" md="4" class="pb-4">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+          
+        </b-col>
+        <b-col cols="12" md="4" class="pb-4">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+          <img class="imagenes" src="../assets/img/logo.png">
+        </b-col>
+      </b-row> 
+      </b-row>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+          <p>
+            <u>
+              <a href="https://www.usfq.edu.ec/vinculacion/Paginas/default.aspx">COCOA USFQ</a>
+            </u> Redes fantasma, impacto ecologico
+          </p>
+          <p class="h6">
+            © Todos los derechos reservados.
+            <a
+              class="text-green ml-2"
+              href="https://www.sunlimetech.com"
+              target="_blank"
+            >USFQ - ECUADOR</a>
+          </p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  </section>
+
+  <!--
+  <b-row id="logos" class="justify-content-md-right" align-v="end" align-h="center">
         <b-col cols="12" md="4">
           <img class="imagenes" src="../assets/img/logo.png">
           <img class="imagenes" src="../assets/img/logo.png">
@@ -13,66 +57,104 @@
           <img class="imagenes" src="../assets/img/logo.png">
           <img class="imagenes" src="../assets/img/logo.png">
         </b-col>
-      </b-row>
+      </b-row> 
+
+  -->
 </template>
 
 <script>
 export default {
-    name: "Foot"
-}
+  name: "Foot"
+};
 </script>
 
 <style scoped>
-#logos{
-  height: 20%;
-  color: #edf2ca;
-  
-
+section {
+  padding: 40px 0;
+  padding-top: 60px;
 }
 
-@media (max-width: 400px){
-  #logos{
+section .section-title {
+  text-align: center;
+  color: #007b5e;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+}
+#footer {
+  background: #03141d;
+}
+#footer h5 {
+  padding-left: 10px;
+  border-left: 3px solid #eeeeee;
+  padding-bottom: 6px;
+  margin-bottom: 20px;
+  color: #ffffff;
+}
+#footer a {
+  color: #ffffff;
+  text-decoration: none !important;
+  background-color: transparent;
+  -webkit-text-decoration-skip: objects;
+}
+
+
+@media (max-width: 767px) {
+  #footer h5 {
+    padding-left: 0;
+    border-left: transparent;
+    padding-bottom: 0px;
+    margin-bottom: 10px;
+  }
+}
+
+#logos {
+  height: 50%;
+  color: #edf2ca;
+}
+
+@media (max-width: 400px) {
+  #logos {
     margin-top: 100px;
   }
 
-  #menu{
+  #menu {
     padding-top: 25px;
   }
 }
 
-#contacto{
+#contacto {
   font-size: 100%;
   padding-bottom: 20px;
 }
 
-.imagenes{
+.imagenes {
   padding-right: 10px;
   padding-bottom: 10px;
-  width: 10%;
+  width: 15%;
 }
 
-@media (max-width: 576px){
-  #logos{
+@media (max-width: 576px) {
+  #logos {
     margin-top: 275px;
   }
 }
 
-@media (min-width: 576px) and (max-width: 768px){
-  #logos{
+@media (min-width: 576px) and (max-width: 768px) {
+  #logos {
     margin-top: 275px;
   }
 
-  #contacto{
+  #contacto {
     padding-bottom: 10px;
   }
 }
 
-@media (min-width: 769px) and (max-width: 1200px){
-  #logos{
+@media (min-width: 769px) and (max-width: 1200px) {
+  #logos {
     margin-top: 600px;
   }
 
-  #contacto{
+  #contacto {
     padding-bottom: 15px;
   }
 }
--- src/views/Secundaria.vue
+++ src/views/Secundaria.vue
@@ -2,153 +2,192 @@
   <div class="pt-5 pr-0 content bodySecu">
     <NavBar></NavBar>
 
+    <div class="pb-5 pt-2" id="grad1">
+      <h1 class="text-center pb-5" id="titulo">TITULO</h1>
+      <div class="container" id="video">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe
+            class="embed-responsive-item"
+            src="https://www.youtube.com/embed/mzJj5-lubeM"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <div class="container pt-5">
+        <h2 class="text-center pt-5 pb-5">DESCRIPCION</h2>
+        <p class="description">
+          Los ALDF se refieren a los desechos de pesca abandonados, perdidos o
+          descardados (en inglés abandoned, lost or discarded fishing gear)
+          tanto en zonas costeras como ambientes submarinos (Macfadyen et al
+          2011). Cuando las redes de pesca se encuentran a la deriva (sin
+          control en su operación por pescadores) pueden seguir atrapando
+          especies marinas y provocar la ¨pesca fantasma¨ (NOAA, 2015). En la
+          década de 1960 se elaborarán redes de pesca utilizando materiales de
+          rápida degradación como el algodón y el cáñamo (Kim et al, 2016).
+          Posteriormente, los avances en la tecnología permitieron mejorar el
+          diseño de las artes de pesca y la materia prima fue cambiando a fibras
+          plásticas de mayor durabilidad y resistencia. Por ejemplo, se
+          elaboraron redes con materiales de poliamida (nylon), polietileno y
+          polipropileno, los cuales se demoran décadas en degradarse (Brandt
+          1984; Stelfox et al, 2016). Esto ha causado que las artes de pesca
+          abandonadas o perdidas se acumulen en grandes cantidades en los
+          océanos y estén causen impactos considerables a corto y largo plazo
+          (Stelfox et al, 2016).
+        </p>
+        <h5 class="p-3">
+          ¿Cuál es el impacto de las redes fantasma en
+          arrecifes rocosos?
+        </h5>
+        <p class="description">
+          Las redes fantasmas pueden seguir atrapando y
+          matando a distintas especies marinas amenazadas y en peligro de
+          extinción como tortugas, peces, invertebrados, aves e inclusive
+          mamíferos marinos (Stelfox et al, 2016). Específicamente, los
+          descartes o desechos de redes fantasmas originados de prácticas
+          pesqueras comerciales y artesanales pueden ocasionar considerables
+          cambios en la dinámica de las comunidades bióticas y ecosistemas
+          marinos (Macfadyen et al 2009; Wilcox etl 2014). Se ha evidenciado que
+          las redes fantasma sirven como trampas de sedimentos, lo cual podría
+          provocar altas tasas de mortalidad en comunidades sensibles como los
+          corales de arrecifes (Risk and Edinger 2011; Erftemeijer et al 2012).
+          Por ejemplo, es conocido que las redes fantasma pueden poner en riesgo
+          a la mayoría de las comunidades de corales que habitan en los
+          arrecifes rocosos; modificando su estructura y crecimiento debido a
+          procesos de fragmentación y fracturación (Risk et al., 2001;
+          Valderrama et al 2018; Figueroa et al 2020). Existe muy poca
+          información sobre la magnitud del impacto de los desechos pesqueros y
+          mucho menos sobre los efectos que pueden causar las distintas artes de
+          pesquerías artesanales cuando se encuentran a la deriva y/o caen en
+          los fondos marinos ( Link et al 2019)
+        </p>
+        <h5 class="p-3">
+          ¿Cuál es la situación actual
+          de la problemática de redes fantasma en el ambiente?
+        </h5>
 
-        <div class="pb-5 pt-2" id="grad1">
-          <h1 class="text-center pb-5" id="titulo">TITULO</h1>
-          <div class="container" id="video">
-            <div class="embed-responsive embed-responsive-16by9">
-              <iframe
-                class="embed-responsive-item"
-                src="https://www.youtube.com/embed/mzJj5-lubeM"
-                allowfullscreen
-              ></iframe>
+        <p class="description">
+          Se estima que 6.4
+          millones de toneladas de desechos son arrojados a los océanos
+          anualmente. Las artes de pesca descartadas o abandonadas conforman más
+          del 10% del volumen total de dichos residuos plásticos (Stelfox et al,
+          2016; Gilman et al, 2016). Una reciente estimación hasta el 2017 sobre
+          desechos pesqueros, indican que 5,7 % de todas las redes, 8,6 % de
+          todas las trampas y 29% de todas las líneas (nylon) pesqueras se han
+          perdido en todos los océanos del mundo (Richardson et al 2019). La
+          cantidad, distribución e impacto de las artes de pesca abandonadas han
+          aumentado sustancialmente en las últimas décadas debido al crecimiento
+          de la actividad pesquera y la transición a materiales sintéticos más
+          duraderos y boyantes para su elaboración (Stelfox et al, 2016;
+          Valderrama et al, 2018). En la región de Sudamérica, existe muy pocos
+          datos reportados sobre las causas de abandono, pérdidas o descartes de
+          desechos pesqueros, lo cual ha limitado la implementación de
+          estrategias de manejo y respuesta ante la problemática (Richardson et
+          al 2019). Existen innumerables causas por las cuales las artes de
+          pesca son abandonadas, pérdidas o descartadas. Por ejemplo, cuando se
+          enganchan en el fondo marino, se rompen a causa de las malas
+          condiciones ambientales, fallas operativos, e inclusive pueden
+          desecharse por la falta de espacio en las embarcaciones pesqueras
+          (Gilman et al, 2016; Richardson et al 2018). Una vez que existe la
+          acumulación de redes fantasma en ecosistemas como los arrecifes rocos,
+          se presenta la dificultad logística en recuperar las redes sin dañar a
+          los corales (Valderrama et al, 2018), incluyendo la recolección y
+          almacenamiento adecuado de las redes pesqueras en tierra (Stelfox et
+          al, 2016;). Por lo tanto, para mitigar los impactos de las redes
+          fantasmas y conservación de los ecosistemas marinos aún es importante
+          el trabajo colaborativo entre ONGs, comunidades locales, agencias
+          gubernamentales, academia, y autoridades pesqueras tanto nacionales y
+          regionales (Richardson et al 2018).
+        </p>
+      </div>
+    </div>
+    <div class="stripe"></div>
+    <div class="parallax">
+      <b-container class="pt-5">
+        <b-jumbotron class="jumbo" header="Conservación" lead="Cuida el mar">
+          <p>Para ams info haz click aqui</p>
+          <b-button variant="primary" href="#">Info</b-button>
+        </b-jumbotron>
+      </b-container>
+    </div>
+
+    <div class="pb-5" id="grad2">
+      <div class="stripe"></div>
+      <h2 style="color:black" class="text-center pt-5 pb-5">GALERIA</h2>
+
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4">
+          <b-button v-b-modal.modal-1>
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-1" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+
+        <div class="col mb-4">
+          <b-button v-b-modal="'modal-2'">
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-2" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+        <div class="col mb-4">
+          <b-button v-b-modal="'modal-3'">
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-3" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+        <div class="col mb-4">
+          <b-button v-b-modal="'modal-4'">
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-4" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+        <div class="col mb-4">
+          <b-button v-b-modal="'modal-5'">
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-5" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+        <div class="col mb-4">
+          <b-button v-b-modal="'modal-6'">
+            <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            <b-modal id="modal-6" title="BootstrapVue">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </b-modal>
+          </b-button>
+        </div>
+      </div>
+
+      <!-- modales-->
+      <div
+        class="modal fade"
+        id="myModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Redes marinas</h5>
             </div>
-          </div>
-          <div class="container pt-5">
-            <h2 class="text-center pt-5 pb-5">DESCRIPCION</h2>
-            <p class="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta
-              vehicula tortor, quis porta lectus dictum non. Curabitur vel
-              molestie leo. Vivamus nisi sapien, porttitor in lorem at, vulputate
-              dapibus erat. Morbi vehicula eros justo, a porttitor velit lacinia
-              sit amet. Donec vehicula eleifend eleifend. Morbi sagittis pharetra
-              quam nec sodales. Donec in nulla suscipit, lobortis felis dictum,
-              laoreet sapien. Fusce maximus tellus a urna fermentum, ut elementum
-              leo rutrum. Vestibulum at diam ac purus imperdiet finibus. Praesent
-              ut felis dolor. Duis pulvinar ipsum vel blandit condimentum. Donec
-              efficitur a risus a malesuada. Praesent bibendum felis ut dictum
-              mattis. Duis at nunc arcu. Duis et eleifend tortor. Mauris
-              consectetur metus eget elit ultrices, et dictum risus viverra. Donec
-              tempor nibh eleifend porttitor dictum. Vivamus maximus nisl leo,
-              eget mattis eros aliquet quis. Sed vestibulum lectus turpis, id
-              malesuada lectus malesuada et. Morbi nec sapien vel libero congue
-              efficitur pellentesque vitae metus. Morbi accumsan ullamcorper nunc
-              sit amet pulvinar. Vestibulum tincidunt, urna sed sollicitudin
-              consectetur, ante orci porta lacus, eget tincidunt urna erat in
-              mauris. Pellentesque sem sem, porta vel lacus malesuada, iaculis
-              malesuada ex. Aliquam accumsan arcu velit, non ultricies lacus
-              malesuada eget. Suspendisse tellus nisl, ullamcorper et faucibus eu,
-              pharetra vel augue. Nullam urna justo, fringilla sit amet lectus ut,
-              malesuada mollis sem. Vestibulum nec pellentesque justo. Etiam non
-              egestas ipsum, eget tempor lacus. Proin egestas, nisi non congue
-              ornare, turpis sem scelerisque nulla, a pellentesque tellus mauris
-              id odio. Maecenas vitae tristique est. Cras at nisl condimentum,
-              malesuada eros sed, ultrices massa. Donec faucibus felis vitae est
-              fringilla mollis at a elit. Aliquam tempor enim nulla. Donec vel
-              mattis nibh. Donec euismod, ante sit amet pulvinar pretium, ante
-              lectus porta nunc, sed porttitor nibh elit quis mauris. Aliquam erat
-              volutpat. Quisque id tincidunt ante. Aenean malesuada lobortis
-              tortor, vel mollis nulla porttitor vel. Cras vel ex in justo rutrum
-              vulputate in vel ex. Sed ultrices mi mollis tempus congue. Vivamus
-              at efficitur metus. Quisque semper nisl sit amet risus consectetur,
-              ornare tincidunt nulla aliquam. Etiam eu nunc elementum, imperdiet
-              dui et, varius dui. Etiam sit amet lacus augue. Aenean malesuada
-              suscipit metus vitae lacinia.
-            </p>
+            <div class="modal-body">
+              <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
+            </div>
           </div>
         </div>
-        <div class="stripe"></div>
-        <div class="parallax">
-          <b-container class="pt-5">
-            <b-jumbotron
-              class="jumbo"
-              header="Conservación"
-              lead="Cuida el mar"
-            >
-              <p>Para ams info haz click aqui</p>
-              <b-button variant="primary" href="#">Info</b-button>
-            </b-jumbotron>
-          </b-container>
-        </div>
-        
-        <div class="pb-5">
-          <div class="stripe"></div>
-          <h2 style="color:black" class="text-center pt-5 pb-5">GALERIA</h2>
-
-          <div class="row row-cols-1 row-cols-md-3">
-            <div class="col mb-4">
-              <b-button v-b-modal.modal-1>
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-1" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-
-            <div class="col mb-4">
-              <b-button v-b-modal="'modal-2'">
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-2" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-            <div class="col mb-4">
-              <b-button v-b-modal="'modal-3'">
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-3" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-            <div class="col mb-4">
-              <b-button v-b-modal="'modal-4'">
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-4" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-            <div class="col mb-4">
-              <b-button v-b-modal="'modal-5'">
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-5" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-            <div class="col mb-4">
-              <b-button v-b-modal="'modal-6'">
-                <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                <b-modal id="modal-6" title="BootstrapVue">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </b-modal>
-              </b-button>
-            </div>
-          </div>
-
-          <!-- modales-->
-          <div
-            class="modal fade"
-            id="myModal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Redes marinas</h5>
-                </div>
-                <div class="modal-body">
-                  <img src="https://via.placeholder.com/400" class="card-img-top" alt="..." />
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- fin modales-->
-        </div>
+      </div>
+      <!-- fin modales-->
+    </div>
 
     <Footer></Footer>
   </div>
@@ -173,6 +212,9 @@ export default {
 #grad1 {
   background-image: linear-gradient(#399ebc, #062d40);
 }
+#grad2 {
+  background-image: linear-gradient(#062d40, #010c13);
+}
 
 #titulo {
   padding-top: 75px;
@@ -183,7 +225,6 @@ export default {
 }
 .content {
   color: white;
-
 }
 .description {
   column-count: 2;
@@ -192,11 +233,11 @@ export default {
   text-justify: inter-word;
 }
 #logos {
-  padding-top: 60px;
+  padding-top: 45px;
   padding-bottom: 40px;
   color: white;
   font-size: 25px;
-  background-color: #154e64;
+  background-color: #03141d;
 }
 .stripe {
   height: 3px;
@@ -229,12 +270,11 @@ export default {
   background-color: rgba(0, 0, 0, 0.37);
   padding-top: 120px;
 }
-.bodySecu
-{
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-    overflow-x: hidden; 
+.bodySecu {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+  overflow-x: hidden;
 }
 </style>
